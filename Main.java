c336917192acc7d22bd5034a42918d08c5d6f7c0
verruu <gacek333@gmail.com>
2023-08-26 21:42:13 +0200

    Simple snake game - working beta v1.0.0.

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         JFrame frame = new JFrame();
         frame.add(new SnakeGame());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
